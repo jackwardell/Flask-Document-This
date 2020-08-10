66fb4dcc3c21f180e94ff47abd417fadb54a35dc
jackwardell <jackwardell@me.com>
2020-08-10 22:13:30 +0100

    adding more to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name="flask_api_docs",
+    name="flask_document_this",
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
